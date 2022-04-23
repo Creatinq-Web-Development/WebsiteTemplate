46a0e690429cbd489b117e486e80c569cd28d736
Creatinq <103510311+Creatinq@users.noreply.github.com>
2022-04-23 14:41:39 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,7 +37,7 @@ function startIntroTyping() {
 function typerStartTyping(typer) {
   typer.reset();
 
-  let text = ['Java Developer', 'Pepsi W', 'Cats are cool', 'Certified skidder', 'bro why are you still here'];
+  let text = ['Line 1', 'Line 2', 'Line 3', 'Line 4', 'Line 5'];
 
   text.forEach(function (language, index) {
     typer.move(null);
@@ -62,7 +62,7 @@ function startMainTyping() {
 }
 
 function switchScreen() {
-  document.title = 'Creatinq on top W';
+  document.title = 'YOUR NAME';
 
   $('.intro').fadeOut(1000, function () {
     $('.bg-image').fadeIn(1000);
